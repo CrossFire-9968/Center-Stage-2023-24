383e3a16778c49fc6642e6846782470b390dd819
Brannon Oldre <brannonoldre@gmail.com>
2023-10-03 19:24:25 -0500

    Fixes mechanum drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotControl.java
@@ -12,23 +12,45 @@ public class RobotControl extends OpMode {
     private DcMotor MotorLFront;
     private DcMotor MotorRFront;
     double driveSpeed;
+    double turnSpeed;
 
     @Override
     public void init( ) {
         MotorLRear = hardwareMap.get(DcMotor.class, "MotorLRear");
-        MotorRRear = hardwareMap.get(DcMotor.class, "MotorRRear");
-        MotorLFront = hardwareMap.get(DcMotor.class, "MotorLFront");
-        MotorRFront = hardwareMap.get(DcMotor.class, "MotorRFront");
-        MotorRRear.setDirection(DcMotorSimple.Direction.REVERSE);
-        MotorLFront.setDirection(DcMotorSimple.Direction.REVERSE);
-        MotorRFront.setDirection(DcMotorSimple.Direction.FORWARD);
         MotorLRear.setDirection(DcMotorSimple.Direction.FORWARD);
+        MotorLRear.setPower(0.0);
+
+        MotorRRear = hardwareMap.get(DcMotor.class, "MotorRRear");
+        MotorRRear.setDirection(DcMotorSimple.Direction.REVERSE);
+        MotorRRear.setPower(0.0);
+
+        MotorLFront = hardwareMap.get(DcMotor.class, "MotorLFront");
+        MotorLFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        MotorLFront.setPower(0.0);
+
+        MotorRFront = hardwareMap.get(DcMotor.class, "MotorRFront");
+        MotorRFront.setDirection(DcMotorSimple.Direction.FORWARD);
+        MotorRFront.setPower(0.0);
+
+
+
     }
 
 
     @Override
     public void loop() {
+        turnSpeed = gamepad1.right_stick_x;
         driveSpeed = -gamepad1.left_stick_y;
+        MotorRFront.setPower(driveSpeed);
+        MotorLFront.setPower(driveSpeed);
+        MotorRRear.setPower(driveSpeed);
+        MotorLRear.setPower(driveSpeed);
+
+        MotorRFront.setPower(-turnSpeed);
+        MotorLFront.setPower(turnSpeed);
+        MotorRRear.setPower(-turnSpeed);
+        MotorLRear.setPower(turnSpeed);
+
         telemetry.addData("speed Forward", driveSpeed);
         telemetry.update();
     }
