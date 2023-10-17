4663ebdeb97b1acbd04584a43b5b383d70dceea3
DESKTOP-MSJPKQI\dawso <dawsonlydiag@gmail.com>
2023-10-17 18:46:40 -0500

    added mecanum drive options to test and limited power to 0.7

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotControl.java
@@ -14,6 +14,9 @@ public class RobotControl extends OpMode {
     double driveSpeed;
     double turnSpeed;
     double strafeSpeed;
+    boolean wasX = true;
+    boolean wasY = false;
+    double driveSensitivity;
 
     @Override
     public void init( ) {
@@ -36,10 +39,64 @@ public class RobotControl extends OpMode {
 
     @Override
     public void loop() {
-        Drive();
+        if (gamepad1.x || wasX) {
+            drive();
+            wasX = true;
+            wasY = false;
+            telemetry.addLine("Drive Normal");
+        }
+        if (gamepad1.y || wasY) {
+            adjustedDrive();
+            wasY = true;
+            wasX = false;
+            telemetry.addLine("Drive Adjusted");
+        }
+
+
+        telemetry.addData("speed Forward", driveSpeed);
+        telemetry.update();
+    }
+    public void drive() {
+        driveSensitivity = 0.7;
+        strafeSpeed = gamepad1.left_stick_x * driveSensitivity;
+        turnSpeed = gamepad1.right_stick_x * driveSensitivity;
+        driveSpeed = -gamepad1.left_stick_y * driveSensitivity;
+
+        double RFrontPower = driveSpeed - turnSpeed - strafeSpeed;
+        double LFrontPower = driveSpeed + turnSpeed + strafeSpeed;
+        double RRearPower = driveSpeed - turnSpeed + strafeSpeed;
+        double LRearPower = driveSpeed + turnSpeed - strafeSpeed;
+
+        double maxPower = driveSensitivity;
+
+        if (RFrontPower > maxPower){
+            maxPower = RFrontPower;
+        }
+        if (LFrontPower > maxPower){
+            maxPower = LFrontPower;
+        }
+        if (RRearPower > maxPower){
+            maxPower = RRearPower;
+        }
+        if (LRearPower > maxPower){
+            maxPower = LRearPower;
+        }
+
+        RFrontPower = (RFrontPower / maxPower);
+        LFrontPower = (LFrontPower / maxPower);
+        RRearPower = (RRearPower / maxPower);
+        LRearPower = (LRearPower / maxPower);
+
+        MotorRFront.setPower(RFrontPower);
+        MotorLFront.setPower(LFrontPower);
+        MotorRRear.setPower(RRearPower);
+        MotorLRear.setPower(LRearPower);
+
+        telemetry.addData("MaxPower:", maxPower);
     }
 
-    public void Drive() {
+    public void adjustedDrive() {
+        driveSensitivity = 0.7;
         strafeSpeed = gamepad1.left_stick_x;
         turnSpeed = gamepad1.right_stick_x;
         driveSpeed = -gamepad1.left_stick_y;
@@ -49,13 +106,37 @@ public class RobotControl extends OpMode {
         double RRearPower = driveSpeed - turnSpeed + strafeSpeed;
         double LRearPower = driveSpeed + turnSpeed - strafeSpeed;
 
+        double RFrontPowerSq = Math.pow(RFrontPower, 3)  * driveSensitivity;
+        double LFrontPowerSq = Math.pow(LFrontPower, 3)  * driveSensitivity;
+        double RRearPowerSq = Math.pow(RRearPower, 3)  * driveSensitivity;
+        double LRearPowerSq = Math.pow(LRearPower, 3)  * driveSensitivity;
+
+        double maxPowerSq = driveSensitivity;
+
+        if (RFrontPowerSq > maxPowerSq){
+            maxPowerSq = RFrontPowerSq;
+        }
+        if (LFrontPowerSq > maxPowerSq){
+            maxPowerSq = LFrontPowerSq;
+        }
+        if (RRearPowerSq > maxPowerSq){
+            maxPowerSq = RRearPowerSq;
+        }
+        if (LRearPowerSq > maxPowerSq){
+            maxPowerSq = LRearPowerSq;
+        }
+
+        RFrontPower = (RFrontPowerSq / maxPowerSq);
+        LFrontPower = (LFrontPowerSq / maxPowerSq);
+        RRearPower =  (RRearPowerSq / maxPowerSq);
+        LRearPower =  (LRearPowerSq / maxPowerSq);
+
         MotorRFront.setPower(RFrontPower);
         MotorLFront.setPower(LFrontPower);
         MotorRRear.setPower(RRearPower);
         MotorLRear.setPower(LRearPower);
 
-        telemetry.addData("speed Forward", driveSpeed);
-        telemetry.update();
+        telemetry.addData("MaxPowerSq:", maxPowerSq);
     }
-}
 
+}
