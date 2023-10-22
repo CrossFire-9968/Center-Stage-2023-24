d4e1dd5572e033d27e99a9a56bda4246391bc96f
DESKTOP-MSJPKQI\dawso <dawsonlydiag@gmail.com>
2023-10-22 14:36:36 -0500

    Created Arm code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmControl.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+public class ArmControl {
+    private DcMotor ArmMotor;
+
+    public void init() {
+        ArmMotor = hardwareMap.get(DcMotor.class, "MotorRFront");
+        ArmMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        //ArmMotor.setMotorType(DcMotor.RunMode.RUN_TO_POSITION);
+        ArmMotor.setPower(0.0);
+        ArmMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        ArmMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+}
+
+    public void drive() {
+        telemetry.addData("Arm Position:", ArmMotor.getCurrentPosition());
+    }
