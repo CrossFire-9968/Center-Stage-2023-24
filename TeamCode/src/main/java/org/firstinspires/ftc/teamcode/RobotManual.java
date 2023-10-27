dd04f09c94d37537b9c6da20879f44a5a1bc506e
DESKTOP-MSJPKQI\dawso <dawsonlydiag@gmail.com>
2023-10-26 19:25:15 -0500

    Adds pixelArm raise and lower

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotManual.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotManual.java
@@ -7,11 +7,12 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp(name="Robot Manual")
 public class RobotManual extends OpMode {
     MecanumDrive mecDrive = new MecanumDrive();
+    PixelArmDropper pixelArm = new PixelArmDropper();
 
     @Override
     public void init() {
         mecDrive.init(hardwareMap);
-
+        pixelArm.init(hardwareMap);
         
         telemetry.addLine("Begin initializations");
         telemetry.addLine("... Mecanum init complete");
@@ -21,6 +22,8 @@ public class RobotManual extends OpMode {
     @Override
     public void loop() {
         mecDrive.manualDrive(gamepad1, telemetry);
+        pixelArm.drive(gamepad1, telemetry);
+        telemetry.addData("Arm position: ", pixelArm.ArmMotor.getCurrentPosition());
         telemetry.update();
     }
 }
