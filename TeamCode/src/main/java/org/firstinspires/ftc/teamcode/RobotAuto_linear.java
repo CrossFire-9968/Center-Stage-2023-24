cf6a6ce239815a5112a43edd809ac4415034f6f4
Brannon Oldre <brannonoldre@gmail.com>
2023-11-13 16:56:47 -0600

    makes some simple aauto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAuto_linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAuto_linear.java
@@ -18,6 +18,7 @@ public class RobotAuto_linear extends LinearOpMode
     double RRearPower;
     double LRearPower;
     final double driveSensitivity = 0.7;
+    boolean isAutoComplete= false;
 
 
     @Override
@@ -26,17 +27,23 @@ public class RobotAuto_linear extends LinearOpMode
         // Declare any local / helper variables here
         // Our initialization code should go here before calling "WaitForStart()"
 
+
         motor_LF = hardwareMap.get(DcMotor.class, "Motor_LF");
-        motor_LF.setDirection(DcMotorSimple.Direction.FORWARD);
+        motor_LF.setDirection(DcMotorSimple.Direction.REVERSE);
+        motor_LF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         motor_RF = hardwareMap.get(DcMotor.class, "Motor_RF");
-        motor_RF.setDirection(DcMotorSimple.Direction.REVERSE);
+        motor_RF.setDirection(DcMotorSimple.Direction.FORWARD);
+        motor_RF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         motor_RR = hardwareMap.get(DcMotor.class, "Motor_RR");
-        motor_RR.setDirection(DcMotorSimple.Direction.FORWARD);
+        motor_RR.setDirection(DcMotorSimple.Direction.REVERSE);
+        motor_RR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         motor_LR = hardwareMap.get(DcMotor.class, "Motor_LR");
-        motor_LR.setDirection(DcMotorSimple.Direction.REVERSE);
+        motor_LR.setDirection(DcMotorSimple.Direction.FORWARD);
+        motor_LR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
 
         setMecanumPowers(0.0);
 
@@ -44,16 +51,34 @@ public class RobotAuto_linear extends LinearOpMode
         waitForStart();
 
         // Run this code while Autonomous has not timed out
-        while (opModeIsActive()) {
-            drive(0.5, 1000);
+        while (opModeIsActive() && !isAutoComplete) {
+
 
             while(!isMotionComplete()){
                 idle();
             }
+
+            strafe(0.5, 52*40);
+
+            while(!isMotionComplete()) {
+                idle();
+            }
+            drive(0.5, 85*32);
+
+            while(!isMotionComplete()){
+                idle();}
+
+            //rotate(0.5, 1000);
+
+            while(!isMotionComplete()) {
+                idle();
+            }
+
+            isAutoComplete = true;
         }
     }
 
-
+// 32 counts per inch est.
     public void drive(double power, int distance) {
         stopAndResetEncoders();
         setMecanumPowers(power);
@@ -61,20 +86,20 @@ public class RobotAuto_linear extends LinearOpMode
         runToPosition();
     }
 
-
+// 39 counts per inch est.
     public void strafe (double power, int distance)
     {
         stopAndResetEncoders();
-        setMecanumPowers(power, -power, power, -power);
-        setTargetPosition(distance);
+        setMecanumPowers(power);
+        setTargetPosition(distance, -distance, distance, -distance);
         runToPosition();
     }
 
 
     public void rotate (double power, int distance) {
         stopAndResetEncoders();
-        setMecanumPowers(power, -power, -power, power);
-        setTargetPosition(distance);
+        setMecanumPowers(power);
+        setTargetPosition(distance, -distance, -distance, distance);
         runToPosition();
     }
 
@@ -129,6 +154,14 @@ public class RobotAuto_linear extends LinearOpMode
         motor_LR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
+
+    private void setTargetPosition(int LF_distance, int RF_distance, int RR_distance, int LR_distance)
+    {
+        motor_LF.setTargetPosition(LF_distance);
+        motor_RF.setTargetPosition(RF_distance);
+        motor_RR.setTargetPosition(RR_distance);
+        motor_LR.setTargetPosition(LR_distance);
+    }
 
     private void setTargetPosition(int distance)
     {
